451617cf9aa6d6542f1f49880249843bd9bde0be
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-10-20 09:08:50 -0300

    add payment

--- src/main/java/com/webservice/webservice/entities/Order.java
+++ src/main/java/com/webservice/webservice/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.webservice.webservice.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +42,12 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	// Keep same ID - one to one relationship
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
+	
+
 	public Order() {
 		
 	}
@@ -52,6 +60,14 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
 	public Set<OrderItem> getItems(){
 		return items;
 	}
